7507bac01c32224096f2f23fb3a12270d4d9d9f3
PinChiChu1217 <yama.chu1217@me.com>
2020-06-25 23:14:41 +0800

    剛剛更新的

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $db_name = "smartfarming";
 
 /* Attempt to connect to MySQL database */
 $link = new mysqli($db_host, $db_user, $db_password, $db_name);
- 
+
 // Check connection
 if($link->connect_errno){
     debug_to_console("Failed to connect to MySQL: (" . $link->connect_errno . ") " . $link->connect_error);
@@ -16,8 +16,47 @@ if($link->connect_errno){
 }
 debug_to_console($link->host_info . "\n");
 
-// Show echo string in Browser Console log 
+// Show echo string in Browser Console log
 function debug_to_console($data) {
     echo "<script>console.log('Debug Objects: " . $data. "' );</script>";
 }
+
+
+  $sql = "SELECT name,class,detail,method FROM job_list";
+  if($result = mysqli_query($link, $sql)){
+      if(mysqli_num_rows($result) > 0){
+          echo "<table>";
+              echo "<tr>";
+                  echo "<th>name</th>";
+                  echo "<th>class</th>";
+                  echo "<th>method</th>";
+              echo "</tr>";
+          while($row = mysqli_fetch_array($result)){
+              echo "<tr>";
+                  echo "<td>" . $row['name'] . "</td>";
+                  echo "<td>" . $row['class'] . "</td>";
+                  echo "<td>" . $row['detail'] . "</td>";
+              echo "</tr>";
+          }
+          echo "</table>";
+          // Free result set
+          mysqli_free_result($result);
+      } else{
+          echo "No records matching your query were found.";
+      }
+  } else{
+      echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+  }
+
+
+
+
+
+
+// Close connection
+mysqli_close($link);
+?>
+
+// Close connection
+mysqli_close($link);
 ?>
